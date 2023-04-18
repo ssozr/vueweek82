c5657d2fd11353db65a147390f82b3c93b8733c7
ssozr <a917382460@gmail.com>
2023-04-19 00:42:43 +0800

    4-19

--- src/components/ArticleHome.vue
+++ src/components/ArticleHome.vue
@@ -70,28 +70,7 @@ body {
                 </div>
             </swiper-slide>
         </swiper>
-        <swiper :modules="modules" :space-between="30" :autoplay="{
-      delay: 2500,
-      disableOnInteraction: false,
-    }"
-    :pagination="{
-      clickable: true,
-    }" class="mySwiper pb-0">
-            <swiper-slide v-for="(article, i) in articlesData.reverse()" :key="article.id">
-                <div class="row align-items-center px-3 py-8 bg-F7F3F0 mb-6 border-bottom border-primary border-4" style="width:100%">
-                    <div class="offset-lg-2 col-xl-1 p-0 col-lg-2 col-sm-3 col-md-2 text-center mb-3">
-                        <img src="@/assets/images/Group.png" alt="女性頭像圖示" v-if="i % 2 === 0">
-                        <img src="@/assets/images/Group2.png" alt="男性頭像圖示" v-else>
-                    </div>
-                    <div class=" p-0 mb-5 col-xl-6 col-lg-5 col-sm-9 col-md-10 fs-5">
-                        <p class="m-0">{{ article.description }}</p>
-                    </div>
-                    <div class="col-lg-3 ps-19 pe-0 d-lg-block d-flex justify-content-end">
-                        <RouterLink :to="`/articles`" class="btn btn-secondary rounded-pill text-white fs-6">更多故事</RouterLink>
-                    </div>
-                </div>
-            </swiper-slide>
-        </swiper>
+
       </div>
 
     </div>
