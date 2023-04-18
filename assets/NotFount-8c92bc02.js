117b2159fca68f8bb52b329ab26db258d81e80c1
ssozr <a917382460@gmail.com>
2023-04-19 01:00:36 +0800

    deploy

---
+++ assets/NotFount-8c92bc02.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as s,c,d as n,w as a,b as r,g as _}from"./index-db8d76b1.js";const d={},i={class:"container"},f=r("p",{class:"fs-1"},"404:錯誤網址",-1);function l(p,u){const e=o("RouterLink");return s(),c("div",i,[f,n(e,{to:"/",class:"fs-1 text-info"},{default:a(()=>[_("點擊返回首頁")]),_:1})])}const x=t(d,[["render",l]]);export{x as default};
